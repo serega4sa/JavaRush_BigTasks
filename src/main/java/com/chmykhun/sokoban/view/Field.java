53927718f8971def3c1308f1d6c633260ba36d35
serega4sa <serega4sa@gmail.com>
2018-08-06 15:04:14 +0300

    [SC-3] Sokoban - added key handler

--- src/main/java/com/chmykhun/sokoban/view/Field.java
+++ src/main/java/com/chmykhun/sokoban/view/Field.java
@@ -1,10 +1,13 @@
 package com.chmykhun.sokoban.view;
 
 import com.chmykhun.sokoban.controller.EventListener;
+import com.chmykhun.sokoban.model.Direction;
 import com.chmykhun.sokoban.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
 
@@ -13,6 +16,8 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        addKeyListener(new KeyHandler());
+        setFocusable(true);
     }
 
     public void setEventListener(EventListener eventListener) {
@@ -25,6 +30,23 @@ public class Field extends JPanel {
         g.fillRect(getX(), getY(), getWidth(), getHeight());
         for (GameObject gameObject : view.getGameObjects().getAll()) {
             gameObject.draw(g);
+        }
+    }
+
+    private class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                eventListener.move(Direction.LEFT);
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                eventListener.move(Direction.RIGHT);
+            } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+                eventListener.move(Direction.UP);
+            } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                eventListener.move(Direction.DOWN);
+            } else if (e.getKeyCode() == KeyEvent.VK_R) {
+                eventListener.restart();
+            }
         }
     }
 }
