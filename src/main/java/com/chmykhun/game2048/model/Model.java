d921b4886f195ebbde4634562a97758bc7bc4cdd
serega4sa <serega4sa@gmail.com>
2018-08-10 16:37:01 +0300

    [SC-4] Game 2048 - implementation of left move logic (test)

--- src/main/java/com/chmykhun/game2048/model/Model.java
+++ src/main/java/com/chmykhun/game2048/model/Model.java
@@ -8,8 +8,14 @@ public class Model {
     private final int FIELD_WIDTH = 4;
 
     private List<List<Tile>> gameTiles;
+    private int score;
+    private int maxTile;
 
     public Model() {
+        initGameTiles();
+    }
+
+    private void initGameTiles() {
         gameTiles = new ArrayList<>();
         for (int x = 0; x < FIELD_WIDTH; x++) {
             List<Tile> tilesColumn = new ArrayList<>();
@@ -18,5 +24,68 @@ public class Model {
             }
             gameTiles.add(tilesColumn);
         }
+    }
+
+    public void resetGameTiles() {
+        initGameTiles();
+    }
+
+    public void left() {
+        boolean isTilesCompressed = false;
+        boolean isTilesMerged = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            List<Tile> tilesLine = new ArrayList<>();
+            for (List<Tile> gameTile : gameTiles) {
+                tilesLine.add(gameTile.get(i));
+            }
+            isTilesCompressed = compressTiles(tilesLine);
+            isTilesMerged = mergeTiles(tilesLine);
+        }
+        if (isTilesCompressed || isTilesMerged) {
+            addTile();
+        }
+    }
+
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        emptyTiles.get((int) (emptyTiles.size() * Math.random())).setValue(Math.random() < 0.9 ? 2 : 4);
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> emptyTiles = new ArrayList<>();
+        for (List<Tile> gameTilesColumn : gameTiles) {
+            for (Tile tile : gameTilesColumn) {
+                if (tile.isEmpty()) {
+                    emptyTiles.add(tile);
+                }
+            }
+        }
+        return emptyTiles;
+    }
+
+    private boolean compressTiles(List<Tile> tiles) {
+        boolean isTilesCompressed = false;
+        for (int i = 1; i <= tiles.size(); i++) {
+            if (tiles.get(i).getValue() > 0 && tiles.get(i - 1).getValue() == 0) {
+                tiles.get(i - 1).setValue(tiles.get(i).getValue());
+                tiles.get(i).setValue(0);
+                isTilesCompressed = true;
+            }
+        }
+        return isTilesCompressed;
+    }
+
+    private boolean mergeTiles(List<Tile> tiles) {
+        boolean isTilesMerged = false;
+        for (int i = 0; i < tiles.size() - 1; i++) {
+            if (tiles.get(i).getValue() > 0 && tiles.get(i).getValue() == tiles.get(i + 1).getValue()) {
+                tiles.get(i).setValue(tiles.get(i).getValue() * 2);
+                tiles.get(i + 1).setValue(0);
+                compressTiles(tiles);
+                isTilesMerged = true;
+                mergeTiles(tiles);
+            }
+        }
+        return isTilesMerged;
     }
 }
--- src/main/java/com/chmykhun/game2048/model/Tile.java
+++ src/main/java/com/chmykhun/game2048/model/Tile.java
@@ -33,6 +33,14 @@ public class Tile {
         this.value = value;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
     public boolean isEmpty() {
         return value == 0;
     }
