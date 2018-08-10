2287af59d66c97ab4a881616dbba961d17ccca41
serega4sa <serega4sa@gmail.com>
2018-08-10 19:06:04 +0300

    [SC-4] Game 2048 - fix of compression and merge logic, implementation of movement in all directions

--- src/main/java/com/chmykhun/game2048/model/Model.java
+++ src/main/java/com/chmykhun/game2048/model/Model.java
@@ -1,6 +1,7 @@
 package com.chmykhun.game2048.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Model {
@@ -30,25 +31,85 @@ public class Model {
         initGameTiles();
     }
 
+    public void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        emptyTiles.get((int) (emptyTiles.size() * Math.random())).setValue(Math.random() < 0.9 ? 2 : 4);
+    }
+
     public void left() {
+        move(Direction.LEFT);
+    }
+
+    public void up() {
+        move(Direction.UP);
+    }
+
+    public void right() {
+        move(Direction.RIGHT);
+    }
+
+    public void down() {
+        move(Direction.DOWN);
+    }
+
+    private void move(Direction direction) {
         boolean isTilesCompressed = false;
         boolean isTilesMerged = false;
         for (int i = 0; i < FIELD_WIDTH; i++) {
-            List<Tile> tilesLine = new ArrayList<>();
-            for (List<Tile> gameTile : gameTiles) {
-                tilesLine.add(gameTile.get(i));
-            }
-            isTilesCompressed = compressTiles(tilesLine);
-            isTilesMerged = mergeTiles(tilesLine);
+            List<Tile> tilesLine = getTilesLine(direction, i);
+            isTilesCompressed |= compressTiles(tilesLine);
+            isTilesMerged |= mergeTiles(tilesLine);
         }
         if (isTilesCompressed || isTilesMerged) {
             addTile();
         }
     }
 
-    private void addTile() {
-        List<Tile> emptyTiles = getEmptyTiles();
-        emptyTiles.get((int) (emptyTiles.size() * Math.random())).setValue(Math.random() < 0.9 ? 2 : 4);
+    /**
+     * Generates list of tiles in one line according to move direction. As compression and merge are performed from the left to the right.
+     *
+     * For example:
+     *      0 2 0 4
+     *      4 0 0 0
+     *      2 0 0 0
+     *      0 0 0 0
+     * resulting list for i=0 will be:
+     *      left:  [0, 2, 0, 4]
+     *      up:    [0, 4, 2, 0]
+     *      right: [4, 0, 2, 0]
+     *      down:  [0, 2, 4, 0]
+     *
+     * @param direction is direction in which performs movement
+     * @param i is index of line
+     * @return list of tiles in one line
+     */
+    private List<Tile> getTilesLine(Direction direction, int i) {
+        List<Tile> tilesLine = new ArrayList<>();
+        switch (direction) {
+            case LEFT: {
+                for (List<Tile> gameTile : gameTiles) {
+                    tilesLine.add(gameTile.get(i));
+                }
+                break;
+            }
+            case UP: {
+                tilesLine = new ArrayList<>(gameTiles.get(i));
+                break;
+            }
+            case RIGHT: {
+                for (List<Tile> gameTile : gameTiles) {
+                    tilesLine.add(gameTile.get(i));
+                }
+                Collections.reverse(tilesLine);
+                break;
+            }
+            case DOWN: {
+                tilesLine = new ArrayList<>(gameTiles.get(i));
+                Collections.reverse(tilesLine);
+                break;
+            }
+        }
+        return tilesLine;
     }
 
     private List<Tile> getEmptyTiles() {
@@ -65,10 +126,11 @@ public class Model {
 
     private boolean compressTiles(List<Tile> tiles) {
         boolean isTilesCompressed = false;
-        for (int i = 1; i <= tiles.size(); i++) {
-            if (tiles.get(i).getValue() > 0 && tiles.get(i - 1).getValue() == 0) {
-                tiles.get(i - 1).setValue(tiles.get(i).getValue());
-                tiles.get(i).setValue(0);
+        for (int i = 0; i < tiles.size() - 1; i++) {
+            if (tiles.get(i).getValue() == 0 && tiles.get(i + 1).getValue() > 0) {
+                tiles.get(i).setValue(tiles.get(i + 1).getValue());
+                tiles.get(i + 1).setValue(0);
+                compressTiles(tiles);
                 isTilesCompressed = true;
             }
         }
@@ -83,7 +145,6 @@ public class Model {
                 tiles.get(i + 1).setValue(0);
                 compressTiles(tiles);
                 isTilesMerged = true;
-                mergeTiles(tiles);
             }
         }
         return isTilesMerged;
